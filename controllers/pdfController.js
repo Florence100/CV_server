bb959d7c17a3593a24f6f38aab0af607fc2e5dae
anastasia <anastasia.gribovskaia@gmail.com>
2024-02-26 22:37:41 +0300

    fix: change styles to file pdf

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,19 +1,14 @@
 import PdfService from '../service/pdfService.js';
 
 class PdfController {
-    createPDF (req, res) {
-        try {
-            res.header('Access-Control-Allow-Origin', '*')
-            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-            res.status(200).json('SERVER IS WORKING')
-            PdfService.createPDF().then(buffer => {
-                res.end(buffer);
-                console.log('buffer', buffer)
-            })
-        } catch(e) {
-            res.status(500).json(e);
-        }
+    async createPDF (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+        PdfService.createPDF(req, res, next).then(buffer => {
+            res.end(buffer)
+        })
     }
 }
 
--- service/pdfService.js
+++ service/pdfService.js
@@ -1,23 +1,19 @@
 import puppeteer from 'puppeteer';
 
 class PdfService {
-    async createPDF(req, res) {
-        const browser = await puppeteer.launch();
+    async createPDF(req, res, next) {
+        const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
-        // const website_url = 'http://127.0.0.1:5501/';
         const website_url = process.env.CLIENT_URL;
         await page.goto(website_url, { waitUntil: 'networkidle0' });
+        await page.addStyleTag({content: '.download-pdf {display: none;} .article.last {padding-top: 10px;} .email:hover {background: green;}'})
         await page.emulateMediaType('screen');
         const buffer = await page.pdf({
-            path: 'CV.pdf',
             margin: { top: '0px', right: '0px', bottom: '0px', left: '0px' },
             printBackground: true,
             format: 'A4',
         });
-        
         await browser.close();
-        // res.end(buffer);
-        console.log('OK!!')
         return buffer;
     }
 }
