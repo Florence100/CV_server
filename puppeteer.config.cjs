ee28a6e120f4c8011b8375bbcdf4a029079970bf
anastasia <anastasia.gribovskaia@gmail.com>
2024-03-18 01:56:59 +0300

    fix: change puppeteer.config.cjs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,11 +1,9 @@
-import { join } from 'path';
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
-*/
-
-const puppeteerConfig = {
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
-
-export default puppeteerConfig;
